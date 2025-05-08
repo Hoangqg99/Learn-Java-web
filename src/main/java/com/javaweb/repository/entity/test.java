4ba16ca96ff07d805aadebf9b92ea06034f5c167
Hoaqg99 <nguyenduyhoang160804@gmail.com>
2025-05-08 17:11:42 +0700

    update

---
+++ src/main/java/com/javaweb/repository/entity/test.java
@@ -0,0 +1,38 @@
+package com.javaweb.repository.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "test")
+public class test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "value")
+    private Integer value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+}
